896203b12ac991455adda6a163322a15e259b093
alvin-kiveu <alvo967@gmail.com>
2022-05-16 12:37:41 +0300

    adding new file

--- test.php
+++ test.php
@@ -1,11 +1,23 @@
 <?php
 include 'config.php';
 $linenumber = "0768168060";
-$checkLoan = mysqli_query($db, "SELECT * FROM aminia_loan WHERE  phonenumber='0768168060' AND loanStatus='Active'");
-$loan = mysqli_fetch_array($checkLoan);
-echo $loan['typeOfFarming'];
-if (mysqli_num_rows($checkLoan) > 0) {
-    echo "It okey";
-} else {
-    echo "It not okey ";
-}
+$messagesent = "AMINIA FARM LOAN APPLICATION You have repaid  Ksh $amountOfAmount of your loan your loan balnce is $newBalance";
+$phone  = "254768168060";
+//send message
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://api.umeskiasoftwares.com/api/v1/sms',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => '{
+        "api_key":"SFhERkJLN0o6NzU0bTYxNGU=",
+        "email":"alvo967@gmail.com",
+        "Sender_Id": "23107",
+        "message": "' .  $messagesent . '",
+        "phone":"' . $phone . '"
+      }',
+    CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json'
+    ),
+));
